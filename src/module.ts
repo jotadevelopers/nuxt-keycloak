7cc929096732e48a78c9c9101f723482283423f1
Josimar Costa <jotadevelopers@gmail.com>
2024-05-18 14:44:04 -0300

    chore: set required keycloak config to module.ts

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,35 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, createResolver } from '@nuxt/kit'
+import defu from 'defu'
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  /**
+   * Keycloak URL
+   * @default process.env.KEYCLOAK_URL
+   * @example 'https://*.mydomain.com'
+   * @type string
+   * @docs https://www.keycloak.org/docs/latest/securing_apps/index.html#_javascript_adapter
+   */
+  url: string
+
+  /**
+   * Keycloak realm
+   * @default process.env.KEYCLOAK_REALM
+   * @example 'myrealm'
+   * @type string
+   * @docs https://www.keycloak.org/docs/latest/securing_apps/index.html#_javascript_adapter
+   */
+  realm: string
+
+  /**
+   * Keycloak realm client-id
+   * @default process.env.KEYCLOAK_CLIENT_ID
+   * @example 'client-id'
+   * @type string
+   * @docs https://www.keycloak.org/docs/latest/securing_apps/index.html#_javascript_adapter
+   */
+  clientId: string
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -10,11 +38,30 @@ export default defineNuxtModule<ModuleOptions>({
     compatibility: { nuxt: '^3.0.0' },
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
+  defaults: {
+    url: process.env.KEYCLOAK_URL as string,
+    realm: process.env.KEYCLOAK_REALM as string,
+    clientId: process.env.KEYCLOAK_CLIENT_ID as string,
+  },
   setup(_options, _nuxt) {
     const { resolve } = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolve('./runtime/plugin'))
+    //Public runtimeConfig
+    _nuxt.options.runtimeConfig.public.keycloak = defu(_nuxt.options.runtimeConfig.public.keycloak, {
+      url: _options.url,
+      realm: _options.realm,
+      clientId: _options.clientId,
+    })
+
+    //Make sure the required configs are configured
+    if (_nuxt.options.runtimeConfig.public.keycloak.url) {
+      console.warn('Missing keycloak url, set it either in `nuxt.config.js` or via env variable')
+    }
+    if (_nuxt.options.runtimeConfig.public.keycloak.realm) {
+      console.warn('Missing keycloak realm, set it either in `nuxt.config.js` or via env variable')
+    }
+    if (_nuxt.options.runtimeConfig.public.keycloak.client) {
+      console.warn('Missing keycloak clientId, set it either in `nuxt.config.js` or via env variable')
+    }
   },
 })
