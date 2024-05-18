610f89a4082b3f91ccba9ffe75cbddcbf3b04086
Josimar Costa <jotadevelopers@gmail.com>
2024-05-18 13:21:29 -0300

    chore: Add .prettierrc

--- src/module.ts
+++ src/module.ts
@@ -1,20 +1,20 @@
-import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: "@jotadevelopers/nuxt-keycloak",
-    configKey: "keycloak",
-    compatibility: { nuxt: "^3.0.0" },
+    name: '@jotadevelopers/nuxt-keycloak',
+    configKey: 'keycloak',
+    compatibility: { nuxt: '^3.0.0' },
   },
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
-    const { resolve } = createResolver(import.meta.url);
+    const { resolve } = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolve("./runtime/plugin"));
+    addPlugin(resolve('./runtime/plugin'))
   },
-});
+})
